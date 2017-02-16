b3a1d059968acd2e798b0754a544a468a3d7449d
Lee Wilkins <lee.wilkins@kartenmacherei.de>
2017-02-16 10:16:40 +0100

    master: added a segment() method to Path

--- src/Library/File/Path.php
+++ src/Library/File/Path.php
@@ -34,13 +34,34 @@ class Path
         return $this->asString() === $path->asString();
     }
 
+    /**
+     * @param Path $path
+     *
+     * @return bool
+     */
     public function contains(Path $path)
     {
         return (strstr($this->asString(), $path->asString()) !== false);
     }
 
+    /**
+     * @param Path $path
+     *
+     * @return bool
+     */
     public function startsWith(Path $path)
     {
         return (substr($this->asString(), 0, strlen($path->asString())) === $path->asString());
+    }
+
+    /**
+     * @param int $index
+     *
+     * @return string
+     */
+    public function segment($index)
+    {
+        $segments = explode('/', trim($this->path, '/'));
+        return $segments[$index];
     }
 }
